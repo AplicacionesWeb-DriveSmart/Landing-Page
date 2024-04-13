f1b97390759bf3dd5c7ab65a43e7a90023733cbb
CarlosG345 <115599345+CarlosG345@users.noreply.github.com>
2024-04-13 01:11:02 -0500

    feat: added javascript code

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,21 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var btnInicio = document.getElementById("button-begin");
+    btnInicio.addEventListener("click", scrollToTop);
+  });
+  
+  // Asociar la funcionalidad al botón Guardar
+  document.addEventListener("DOMContentLoaded", function() {
+    var btnGuardar = document.getElementById("submit-button");
+    btnGuardar.addEventListener("click", function() {
+      var username = document.getElementById("text").value;
+      var password = document.getElementById("password").value;
+      
+  
+      almacenarUsuario(username, password);
+    });
+  });
 
